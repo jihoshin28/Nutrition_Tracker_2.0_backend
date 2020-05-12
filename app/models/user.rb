6ec0c642002db20067bed80635b656c49d2c7c2f
Allen Shin <geeuho@gmail.com>
2020-05-11 23:34:30 -0700

    added username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     has_many :weights
     has_secure_password
     validates :username, presence: true
+    validates :username, uniqueness: true
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :age, presence: true
